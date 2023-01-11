330e3189d5a3f0cf0450e9c94fe884513dff7791
黃士桓 <huan5678@gmail.com>
2023-01-11 23:30:37 +0800

    [update] fixed command

--- models/openAI_Client.py
+++ models/openAI_Client.py
@@ -1,119 +1,113 @@
+import json
 import os
 
 import openai
 
 
 class OpenAI_Client:
-  def __init__(self, model='text-davinci-003', temperature=1, max_tokens=128, frequency_penalty=0.8):
-    openai.organization = os.getenv('OPENAI_ORGANIZATION')
-    openai.api_key = os.getenv('OPENAI_API_KEY')
-    self.model = model
-    self.temperature = temperature
-    self.max_tokens = max_tokens
-    self.frequency_penalty = frequency_penalty
-    self.history = ''
+    def __init__(self, model='text-davinci-003', temperature=1, max_tokens=256, frequency_penalty=0.8):
+        openai.organization = os.getenv('OPENAI_ORGANIZATION')
+        openai.api_key = os.getenv('OPENAI_API_KEY')
+        self.model = model
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.frequency_penalty = frequency_penalty
+        self.history = []
 
-  def generate_prompt(self, message, history):
-    if history == None:
-      return '''
-    Human: {}
-    AI:
-    '''.format(
-          message.capitalize()
-      )
-    else:
-      return '''
-    AI: {}
-    '''.format(
-      message.capitalize(),
-    )
+    def generate_prompt(self, message, history):
+        if len(history) == 0:
+            return '''
+          {}
+    '''.format(message)
+        else:
+            return '''
+    {}
+    '''.format(history)
 
-  def setModel(self, model):
-    self.model = model
+    def generateContinuePrompt(self, history):
+        if len(history) == 0:
+            return
+        else:
+            return '''
+    {}
+    continue...
+    '''.format(history)
 
-  def setTemperature(self, temperature):
-    self.temperature = temperature
+    def setModel(self, model):
+        self.model = model
 
-  def setMaxTokens(self, max_tokens):
-    self.max_tokens = max_tokens
+    def setTemperature(self, temperature):
+        self.temperature = temperature
 
-  def setFrequencyPenalty(self, frequency_penalty):
-    self.frequency_penalty = frequency_penalty
+    def setMaxTokens(self, max_tokens):
+        self.max_tokens = max_tokens
 
-  def setHistory(self, message, response):
-    if message == None:
-      self.history += '''
-      {}
-      '''.format(
-          response['msg'].capitalize()
-      )
-    else:
-      self.history += '''
-      Human: {}
-      AI: {}
-      '''.format(
-            message.capitalize(),
-            response['msg'].capitalize()
-        )
+    def setFrequencyPenalty(self, frequency_penalty):
+        self.frequency_penalty = frequency_penalty
 
-  def getStatus(self):
-    return {
-      "model": self.model,
-      "temperature": self.temperature,
-      "max_tokens": self.max_tokens,
-      "frequency_penalty": self.frequency_penalty,
-    }
+    def setHistory(self, message, response):
+        self.history.append(message)
+        self.history.append(response['msg'])
 
-  def clearHistory(self):
-    self.history = ''
+    def getHistory(self):
+        return self.history
 
-  def getModelList(self):
-    models = openai.Model.list()
-    result = []
-    for model in models.data:
-      result.append(model.id)
-    return result
+    def clearHistory(self):
+        self.history = []
 
-  def generateMessage(self, message):
-      response = openai.Completion.create(
-        model=self.model,
-        prompt=self.generate_prompt(str(message), self.history),
-        temperature=self.temperature,
-        max_tokens=self.max_tokens,
-        frequency_penalty=self.frequency_penalty,
-        )
-      resArr = response.choices[0].text.split('\n')
-      resArr.pop(0)
-      result = ''
-      for resStr in resArr:
-        result += resStr + '\n'
-      print('res = {}'.format(response.choices[0]))
-      resultData = {
-        "msg": result,
-        "isFinished": True if response.choices[0].finish_reason == 'stop' else False,
-      }
-      return resultData
-  
-  def getError(self, e):
-    print(f'errorMessage: {e}')
-    msg = '我們中出了錯誤'
-    return msg
-  
-  def send_message(self, message) -> dict:
-    try:
-      response = self.generateMessage(message)
-      print('send message get response: ', response)
-      self.setHistory(message, response)
-      print('history: ', self.history)
-      return response
-    except Exception as e:
-      self.getError(e)
+    def getStatus(self):
+        return {
+          "model": self.model,
+          "temperature": self.temperature,
+          "max_tokens": self.max_tokens,
+          "frequency_penalty": self.frequency_penalty,
+        }
 
-  def send_continue(self) -> dict:
-    try:
-      response = self.generateMessage(self.history)
-      self.setHistory(None, response)
-      print('history: ', self.history)
-      return response
-    except Exception as e:
-      self.getError(e)
+    def getModelList(self):
+        models = openai.Model.list()
+        result = []
+        for model in models.data:
+            result.append(model.id)
+        return result
+
+    def generateMessage(self, message, userId, isContinue=False):
+        prompt = self.generateContinuePrompt(self.history[-1]) if isContinue else self.generate_prompt(str(message), self.history[-1] if len(self.history) > 0 else '')
+        response = openai.Completion.create(
+          model=self.model,
+          prompt=prompt,
+          temperature=self.temperature,
+          max_tokens=self.max_tokens,
+          frequency_penalty=self.frequency_penalty,
+          user=str(userId),
+          )
+        resArr = response.choices[0].text.split('\n')
+        resArr.pop(0)
+        result = ''
+        for resStr in resArr:
+            result += resStr + '\n'
+        resultData = {
+          "msg": result,
+          "isFinished": True if response.choices[0].finish_reason == 'stop' else False,
+        }
+        return resultData
+
+    def getError(self, e):
+        print(f'errorMessage: {e}')
+        msg = '我們中出了錯誤'
+        return msg
+
+    def send_message(self, message, userId) -> dict:
+        try:
+            response = self.generateMessage(message, userId)
+            self.setHistory(message, response)
+            return response
+        except Exception as e:
+            self.getError(e)
+
+    def send_continue(self, userId) -> dict:
+        try:
+            response = self.generateMessage(self.history[-1], userId, True)
+            self.setHistory(None, response)
+            return response
+        except Exception as e:
+            self.getError(e)
--- skills/openAI_Control.py
+++ skills/openAI_Control.py
@@ -8,64 +8,63 @@ from skills import add_skill
 client = OpenAI_Client()
 
 def continueButton():
-  return TemplateSendMessage(alt_text='繼續命令提示',
-                            template=ButtonsTemplate(
-                              title='繼續',
-                              text='還有未顯示的訊息',
-                              actions=[
-                                MessageAction(label='繼續', text='繼續')
-                              ]))
+    return TemplateSendMessage(alt_text='繼續命令提示',
+                              template=ButtonsTemplate(
+                                title='繼續',
+                                text='還有未顯示的訊息',
+                                actions=[
+                                  MessageAction(label='繼續', text='繼續')
+                                ]))
 
 def generate_message(props_text):
-  return TextSendMessage(text=f'{props_text}')
+    return TextSendMessage(text=f'{props_text}')
 
 def create_prompt(resData):
-  print('resData: ', resData)
-  msg = generate_message(resData['msg'])
-  if resData['isFinished'] != True:
-    btn = continueButton()
-    return [
-      msg,
-      btn
-    ]
-  else:
-    return [
-      msg
-    ]
+    msg = generate_message(resData['msg'])
+    if resData['isFinished'] != True:
+        btn = continueButton()
+        return [
+          msg,
+          btn
+        ]
+    else:
+        return [
+          msg
+        ]
 
 @add_skill('{not_match}')
 def get(message_request: MessageRequest):
-  text = message_request.message
-  resData = client.send_message(text)
-  print('resData: ', resData)
-  result = create_prompt(resData)
-  print('send_result: ', result)
-  return result
+    userId = message_request.user_id
+    text = message_request.message
+    resData = client.send_message(text, userId)
+    result = create_prompt(resData)
+    return result
 
 
 @add_skill('繼續')
 def get(message_request: MessageRequest):
-  resData = client.send_continue()
-  print('resData: ', resData)
-  result = create_prompt(resData)
-  print('send_result: ', result)
-  return result
+    UserId = message_request.user_id
+    resData = client.send_continue(UserId)
+    if len(resData['msg']) == 0:
+        return [TextSendMessage(text='無法辨識')]
+    result = create_prompt(resData)
+    return result
 
 @add_skill('/重來')
 def get(message_request: MessageRequest):
-  client.clearHistory()
-  exit()
+    client.clearHistory()
+    exit()
 
 @add_skill('/token')
 def get(message_request: MessageRequest):
-  text = message_request.message.strip()
-  token = text.split(' ')
-  client.setMaxTokens(int(token[1]))
-  exit()
+    text = message_request.message.strip()
+    token = text.split(' ')
+    client.setMaxTokens(int(token[1]))
+    exit()
 
 @add_skill('/狀態')
 def get(message_request: MessageRequest):
-  return [TextSendMessage(text=f'狀態: {client.getStatus()}')]
+    return [TextSendMessage(text=f'狀態: {client.getStatus()}')]
 
 @add_skill('/model')
 def get(message_request: MessageRequest):
@@ -74,8 +73,14 @@ def get(message_request: MessageRequest):
 
 @add_skill('/setModel')
 def get(message_request: MessageRequest):
-  text = message_request.message.strip()
-  model = text.split(' ')
-  client.setModel(model[1])
-  exit()
+    text = message_request.message.strip()
+    model = text.split(' ')
+    client.setModel(model[1])
+    exit()
 
+@add_skill('/紀錄')
+def get(message_request: MessageRequest):
+    result = client.getHistory()
+    if len(result) == 0:
+        result = '無紀錄'
+    return [TextSendMessage(text=f"{result}")]
